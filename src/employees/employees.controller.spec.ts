a6d3469a594e2d69a61d93174370bad6f4c7d9c2
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2024-05-04 10:42:23 +0100

    COMMIT

--- src/employees/employees.controller.spec.ts
+++ src/employees/employees.controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
